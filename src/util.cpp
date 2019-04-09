f32e956003eeffa6a4caf6d6c94a6cdcfb820517
StephenCzarnecki <sczarnecki@lbl.gov>
2019-04-09 16:16:18 -0400

    Disabling pragma warning push for non-msvc compilers.

--- src/util.cpp
+++ src/util.cpp
@@ -9,9 +9,13 @@ std::string to_lower(std::string s)
 		with if this ever needs to really be internationalized.
 		Until then this should be good enough
 	*/
+#ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable:4244)
+#endif
 	std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+#ifdef _MSC_VER
 #pragma warning(pop)
+#endif
 	return s;
 }
