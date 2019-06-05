69baa1bb4f1e48f689e8a4a30a95c72e919039b9
StephenCzarnecki <sczarnecki@lbl.gov>
2019-06-05 13:49:37 -0400

    Changing name to be entire filename including extension.

--- src/load_standard.cpp
+++ src/load_standard.cpp
@@ -138,10 +138,9 @@ std::vector<Method_Text> convert(std::vector<std::vector<std::string>> const & m
     return converted_methods;
 }
 
-std::string get_file_name_without_extension(std::string const& path)
+std::string get_file_name(std::string const& path)
 {
 	std::string fname = path.substr(path.find_last_of("\\/")+1);
-	fname = fname.substr(0, fname.rfind("."));
 	return fname;
 }
 
@@ -155,7 +154,7 @@ Standard load_standard(std::string const & path)
     std::vector<std::string> method_block;
 
     Standard standard;
-	standard.name = get_file_name_without_extension(path);
+	standard.name = get_file_name(path);
 	
 	std::ifstream input(path);
 
